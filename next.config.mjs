f296b0f57fee0e31af7efc1e5715aee79c847be0
On Par Marketing <onparmarketing@Haley-MacBook-Air.local>
2026-06-19 14:16:00 -0400

    Bypass Cloudflare cache for dashboard script

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/dashboard.js",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
